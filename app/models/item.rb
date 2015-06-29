04ffc8683f7438a7c6d2f118822acf4b91785547
AlexanderPower <aleksandr.power88@gmail.com>
2015-06-29 11:56:07 +0500

    Assets compile.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :category
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   accepts_nested_attributes_for :photos, :allow_destroy => true
 
